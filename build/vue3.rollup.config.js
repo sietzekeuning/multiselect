18ab04360f097dcd801968445bac841770d30b6a
Sietze Keuning <info@kingwebsites.nl>
2021-09-15 12:46:39 +0200

    commonjs

--- build/vue3.rollup.config.js
+++ build/vue3.rollup.config.js
@@ -4,45 +4,43 @@ import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 
 export default [
-  {
-    input: 'src/Multiselect.vue',
-    output: {
-      file: 'dist/multiselect.js',
-      format: 'esm',
+    {
+        input: 'src/Multiselect.vue',
+        output: {
+            file: 'dist/multiselect.js',
+            format: 'commonjs',
+        },
+        plugins: [
+            vue(),
+            babel({
+                babelHelpers: 'runtime',
+                skipPreflightCheck: true,
+            }),
+            alias({
+                entries: [{ find: 'composition-api', replacement: 'vue' }],
+            }),
+            terser(),
+        ],
+        external: 'vue',
     },
-    plugins: [
-      vue(),
-      babel({
-        babelHelpers: 'runtime',
-        skipPreflightCheck: true,
-      }),
-      alias({
-        entries: [
-          { find: 'composition-api', replacement: 'vue' },
-        ]
-      }),
-      terser(),
-    ],
-    external: 'vue',
-  },
-  {
-    input: 'src/Multiselect.vue',
-    output: {
-      file: 'dist/multiselect.global.js',
-      format: 'iife',
-      name: 'VueformMultiselect',
-      globals: {
-        'composition-api': 'Vue',
-        'vue': 'Vue',
-      }
+    {
+        input: 'src/Multiselect.vue',
+        output: {
+            file: 'dist/multiselect.global.js',
+            format: 'iife',
+            name: 'VueformMultiselect',
+            globals: {
+                'composition-api': 'Vue',
+                vue: 'Vue',
+            },
+        },
+        plugins: [
+            vue(),
+            babel({
+                babelHelpers: 'bundled',
+            }),
+            terser(),
+        ],
+        external: ['composition-api', 'vue'],
     },
-    plugins: [
-      vue(),
-      babel({
-        babelHelpers: 'bundled',
-      }),
-      terser()
-    ],
-    external: ['composition-api', 'vue'],
-  }
 ]
